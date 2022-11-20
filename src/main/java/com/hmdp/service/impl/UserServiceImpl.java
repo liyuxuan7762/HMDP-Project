4512f338e6a6674d2559831d35f0f5cd82d7c3ae
Li <776239423@qq.com>
2022-11-20 22:44:09 +0800

    DAY 2 1. 学习使用Redis替代Session实现用户登陆来解决集群之间Session不能共享的问题

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,21 +1,29 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.RandomUtil;
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.SystemConstants.*;
-
+import static com.hmdp.utils.RedisConstants.*;
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -25,54 +33,87 @@ import static com.hmdp.utils.SystemConstants.*;
  * @author 虎哥
  * @since 2021-12-22
  */
-@Service
 @Slf4j
+@Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+
     @Override
     public Result sendCode(String phone, HttpSession session) {
-        // 1.验证手机号是否合法
+        //1. 校验手机号
         if (RegexUtils.isPhoneInvalid(phone)) {
-            // 2.如果不符合，则返回错误信息
-            return Result.fail("手机号码格式不正确");
+            //2.如果不符合，返回错误信息
+            return Result.fail("手机号格式错误");
         }
-        // 3.符合则生成验证码并放入session
+        //3. 符合，生成验证码
         String code = RandomUtil.randomNumbers(6);
-        session.setAttribute(USER_CODE, code);
-        // 4.发送验证码
-        log.info("验证码发送成功，验证码为" + code);
-        // 5.返回结果信息
+        //4. 将验证码保存到Redis
+        // session.setAttribute("code", code);
+        stringRedisTemplate.opsForValue().set(LOGIN_CODE_KEY + phone, code, LOGIN_CODE_TTL, TimeUnit.MINUTES);
+        //5. 发送验证码
+        System.out.println(code);
+        log.debug("发送短信验证码成功，验证码:{}", code);
+        //返回ok
         return Result.ok();
     }
 
     @Override
     public Result login(LoginFormDTO loginForm, HttpSession session) {
-        // 1.验证手机号是否合法
-        if (RegexUtils.isPhoneInvalid(loginForm.getPhone())) {
-            // 2.如果不符合，则返回错误信息
-            return Result.fail("手机号码格式不正确");
+        //1. 校验手机号
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号格式错误");
         }
-        // 2.判断验证码是否相同
-        Object sessionCode = session.getAttribute(USER_CODE);
-        if (loginForm.getCode() == null || !sessionCode.toString().equals(loginForm.getCode())) {
+        //2. 校验验证码
+        // TODO 2.1 从Redis中获取验证码 key为手机号
+        // Object cacheCode = session.getAttribute("code");
+        String cacheCode = stringRedisTemplate.opsForValue().get(phone);
+        String code = loginForm.getCode();
+        if (cacheCode == null || !cacheCode.equals(code)) {
+            //3. 不一致，报错
             return Result.fail("验证码错误");
         }
-        // 3.根据手机号查询用户
-        User user = super.getOne(new LambdaQueryWrapper<User>().eq(User::getPhone, loginForm.getPhone()));
+
+        //4.一致，根据手机号查询用户
+        User user = query().eq("phone", phone).one();
+
+        //5. 判断用户是否存在
         if (user == null) {
-            // 4.如果用户不存在则创建用户
-            user = createUserWithPhone(loginForm.getPhone());
+            //6. 不存在，创建新用户
+            user = createUserWithPhone(phone);
         }
-        // 5.将用户保存到session
-        session.setAttribute(USER_SESSION, user);
-        // 6.返回相应信息
-        return Result.ok();
+
+        // 使用UUID生成Token
+        String token = UUID.fastUUID().toString(true);
+        // 将用户转化为Map
+        UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
+        // 因为我们保存的到Redis的时候使用的是String类型，然而在userDto中ID是Long类型，因此会报错
+        // 使用setFieldValueEditor将userDto所有字段都转化成String类型，然后保存
+        Map<String, Object> userMap = BeanUtil.beanToMap(userDTO, new HashMap<>(), CopyOptions.create()
+                .setIgnoreNullValue(true)
+                .setFieldValueEditor((fieldName, fieldValue) -> fieldValue.toString()));
+        // 将Map以Hash形式保存到Redis
+        String tokenKey = LOGIN_USER_KEY + token;
+        stringRedisTemplate.opsForHash().putAll(tokenKey, userMap);
+
+        // 设置Key有效期
+        stringRedisTemplate.expire(tokenKey, LOGIN_USER_TTL, TimeUnit.SECONDS);
+
+        // session.setAttribute("user", BeanUtil.copyProperties(user, UserDTO.class));
+        return Result.ok(token);
     }
 
+
     private User createUserWithPhone(String phone) {
+        // 1.创建用户
         User user = new User();
         user.setPhone(phone);
         user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
-        super.save(user);
+        // 2.保存用户
+        save(user);
         return user;
     }
 }
